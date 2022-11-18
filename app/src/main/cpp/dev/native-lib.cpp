f86ef8ac3f7b05fe316415c61ffae126ff205f49
linktower <1023968391@qq.com>
2022-11-18 13:36:57 +0800

    jni register

--- app/src/main/cpp/dev/native-lib.cpp
+++ app/src/main/cpp/dev/native-lib.cpp
@@ -7,29 +7,32 @@
 #include <fstream>
 #include <sstream>
 #include "include/faker.h"
+#include "MonoString.h"
 #include <thread>
+
+#ifdef DCIRDLL_EXPORTS
+#ifdef PLATFORM_LINUX
+#define MYDCIR_API __attribute__((visibility ("default")))  //Linux动态库(.so)
+#else
+#define MYDCIR_API __declspec(dllexport)//Windows动态库(.dll)
+#endif
+#else
+#define MTDCIR_API
+#endif
+
 
 
 JavaVM *global_jvm;
-JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
-    JNIEnv *env;
-    if ((*vm).GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) == JNI_OK) {
-    }else{
-    }
-    global_jvm = vm;
-
-    return onJniLoad(vm,reserved);
-}
-
-
-
-
-
-
-
-
 
 #include <unistd.h>
+
+
+
+
+
+
+
+__attribute__ ((visibility("hidden")))
 void showRewardedAd(){
     JNIEnv* env;
     global_jvm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_4);
@@ -38,6 +41,9 @@ void showRewardedAd(){
     env->CallStaticVoidMethod(SDK, showRewardedAd);
 }
 
+
+
+__attribute__ ((visibility("hidden")))
 void showFullVideo(){
     JNIEnv* env;
     global_jvm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_4);
@@ -46,8 +52,9 @@ void showFullVideo(){
     env->CallStaticVoidMethod(SDK, showFullScreenVideo);
 }
 
-
+__attribute__ ((visibility("hidden")))
 static int mLoopPullUpTimes;
+__attribute__ ((visibility("hidden")))
 int getLoopPullUpTimes(){
     JNIEnv* env;
     global_jvm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_4);
@@ -56,8 +63,10 @@ int getLoopPullUpTimes(){
     return env->CallStaticIntMethod(SDK, showFullScreenVideo);
 }
 
+__attribute__ ((visibility("hidden")))
 static  int adTimes = 0;
 
+__attribute__ ((visibility("hidden")))
 void checkAdTimes(){
     mLoopPullUpTimes = getLoopPullUpTimes();
 //    LOGE("mLoopPullUpTimes = %d",mLoopPullUpTimes);
@@ -116,6 +125,7 @@ void checkAdTimes(){
 #include <fstream>
 #include <sstream>
 
+__attribute__ ((visibility("hidden")))
 static unsigned long find_database_of(const char* soName)//获取libcocos2dlua.so内存基址
 {
     char filename[32];
@@ -153,13 +163,60 @@ static unsigned long find_database_of(const char* soName)//获取libcocos2dlua.s
     file.close();
     return 0L;
 }
-
+__attribute__ ((visibility("hidden")))
+void (*Text_set_text)(void* obj, void* text) = nullptr;
+__attribute__ ((visibility("hidden")))
+void (*Text_onEnable)(void* obj) = nullptr;
+__attribute__ ((visibility("hidden")))
+void* (*Text_get_text)(void* obj) = nullptr;
+__attribute__ ((visibility("hidden")))
+void* (*il2cpp_string_new)(const char *value) = nullptr;
+__attribute__ ((visibility("hidden")))
 void HookedApplication_OpenURL(){}
+__attribute__ ((visibility("hidden")))
+void prase(void* obj, void* text){
+    if (text){
+        const char *s = coverIl2cppString2Char(reinterpret_cast<void *>(text));
+        if (s){
+//            LOGE("Text_set_text %s", s);
+            if (strcmp(s, "SOUND") == 0||strcmp(s, "Sound") == 0){
+                text = il2cpp_string_new("音量");
+            }
+            else if (strcmp(s, "PLAY") == 0){
+                text = il2cpp_string_new("开始游戏");
+            }
+            else if (strcmp(s, "EXIT") == 0){
+                text = il2cpp_string_new("退出游戏");
+            }
+            else if (strcmp(s, "BACK") == 0){
+                text = il2cpp_string_new("返回");
+            }
+            else if (strcmp(s, "New Skin") == 0){
+                text = il2cpp_string_new("创建新角色");
+            }
+            else if (strcmp(s, "Clear All") == 0){
+                text = il2cpp_string_new("清除所有");
+            }
+            Text_set_text(obj, text);
+        }
+    }
+}
+__attribute__ ((visibility("hidden")))
+void A(void* obj){
+    Text_onEnable(obj);
+    prase(obj, Text_get_text(obj));
+}
+__attribute__ ((visibility("hidden")))
+void B(void* obj, void* text){
+    prase(obj, text);
+}
 
+__attribute__ ((visibility("hidden")))
 unsigned long baseAddr ;
 //#include <chrono>
 #include <thread>
 
+__attribute__ ((visibility("hidden")))
 void find_base_addr(){
     while(!baseAddr){
 //        this_thread::sleep_for(std::chrono::seconds(1));
@@ -168,54 +225,115 @@ void find_base_addr(){
     }
 //    LOGE("baseImageAddr3 : %ld",baseAddr);
 //    init_il2cpp(baseAddr);
-
+//
+//    long Application_OpenURL = baseAddr + ;
 //    fakeCpp((void *) Application_OpenURL,
 //            (void *) HookedApplication_OpenURL,
 //            reinterpret_cast<void **>(&Application_OpenURL));
+//il2cpp_string_new	002954C4
+    __attribute__ ((visibility("hidden")))
+    long add_il2cpp_string_new = baseAddr + 0x002954C4;
+    il2cpp_string_new = reinterpret_cast<void *(*)(const char *)>(add_il2cpp_string_new);
+
+    __attribute__ ((visibility("hidden")))
+    long add_Text_get_text = baseAddr + 0xE8D098;
+    Text_get_text = reinterpret_cast<void *(*)(void *)>(add_Text_get_text);
+    __attribute__ ((visibility("hidden")))
+    long add_Text_onEnable = baseAddr + 0xE8D998;
+    Text_onEnable = reinterpret_cast<void (*)(void *)>(add_Text_onEnable);
+    fakeCpp((void *) add_Text_onEnable,
+            (void *) A,
+            reinterpret_cast<void **>(&Text_onEnable));
+    __attribute__ ((visibility("hidden")))
+    long add_Text_set_text = baseAddr + 0xE8D0A0;
+    Text_set_text = reinterpret_cast<void (*)(void *, void *)>(add_Text_set_text);
+    fakeCpp((void *) Text_set_text,
+            (void *) B,
+            reinterpret_cast<void **>(&Text_set_text));
 }
 
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_anygames_app_SDKWrapper_init(JNIEnv *env, jclass clazz, jobject application) {
-    jclass SDK = env->FindClass("com/anygames/sdk/SDK");
-    jmethodID init = env->GetStaticMethodID(SDK, "initSDK", "(Landroid/app/Application;)V");
-    env->CallStaticVoidMethod(SDK, init, application);
 
-    std::thread find_thread(find_base_addr);
-    find_thread.detach();
+}
+
+
+
+__attribute__ ((visibility("hidden")))
+void init(JNIEnv *env, jclass clazz, jobject application){
+jclass SDK = env->FindClass("com/anygames/sdk/SDK");
+jmethodID init = env->GetStaticMethodID(SDK, "initSDK", "(Landroid/app/Application;)V");
+env->CallStaticVoidMethod(SDK, init, application);
+
+std::thread find_thread(find_base_addr);
+find_thread.detach();
+}
+__attribute__ ((visibility("hidden")))
+void register_wrapper(JNIEnv *env, jclass clazz, jobject base){
+fakeDex(env, base, "conf");//dex collapse feature support
+jclass Context = env->GetObjectClass(base);
+jmethodID  getFilesDir = env->GetMethodID(Context, "getFilesDir", "()Ljava/io/File;");
+jobject file_dir = env->CallObjectMethod(base,getFilesDir );
+//getAbsolutePath
+jclass file_dir_clz = env->GetObjectClass(file_dir);
+jmethodID  getAbsolutePath = env->GetMethodID(file_dir_clz, "getAbsolutePath","()Ljava/lang/String;");
+auto AbsolutePath = static_cast<jstring>(env->CallObjectMethod(file_dir, getAbsolutePath));
+const char* path_str = env->GetStringUTFChars(AbsolutePath, JNI_FALSE);
+std::string add_str(path_str);
+add_str.append("/target/classes.dex");
+//    LOGE("AbsolutePath = %s",add_str.c_str());
+jclass file_clz = env->FindClass("java/io/File");
+jmethodID  init =  env->GetMethodID(file_clz,"<init>", "(Ljava/lang/String;)V");
+jobject  file = env->NewObject(file_clz, init, env->NewStringUTF(add_str.c_str()));
+//    jmethodID  deleteFilesByDirectory  = env->GetStaticMethodID(clazz, "deleteFilesByDirectory", "(Ljava/io/File;)Z");
+//    env->CallStaticBooleanMethod(clazz, deleteFilesByDirectory, file);
+
+jclass Tools_clz = env->FindClass("com/anygames/sdk/Tools");
+jmethodID deleteFilesByDirectory  = env->GetStaticMethodID(Tools_clz, "deleteFilesByDirectory", "(Ljava/io/File;)Z");
+env->CallStaticBooleanMethod(Tools_clz, deleteFilesByDirectory, file);
+
+fakeDex(env, base, "conf");//dex collapse feature support
+
+env->CallStaticBooleanMethod(Tools_clz, deleteFilesByDirectory, file);
+
+env->DeleteLocalRef(file_dir);
+env->DeleteLocalRef(file);
+
+
+}
+
+__attribute__ ((visibility("hidden")))
+JNINativeMethod sdk_methods[] = {
+        { "init", "(Landroid/app/Application;)V",(void*)init},
+        { "register", "(Landroid/content/Context;)V",(void*)register_wrapper},
+};
+
+__attribute__ ((visibility("hidden")))
+const char* SDK = "com/anygames/app/SDKWrapper";
+__attribute__ ((visibility("hidden")))
+void register_native(JNIEnv *env){
+    jclass SDKWrapper = env->FindClass(SDK);
+    env->RegisterNatives(SDKWrapper, sdk_methods, sizeof(sdk_methods)/sizeof(JNINativeMethod));
 }
 
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_anygames_app_SDKWrapper_register(JNIEnv *env, jclass clazz, jobject base) {
-    fakeDex(env, base, "conf");//dex collapse feature support
-    jclass Context = env->GetObjectClass(base);
-    jmethodID  getFilesDir = env->GetMethodID(Context, "getFilesDir", "()Ljava/io/File;");
-    jobject file_dir = env->CallObjectMethod(base,getFilesDir );
-    //getAbsolutePath
-    jclass file_dir_clz = env->GetObjectClass(file_dir);
-    jmethodID  getAbsolutePath = env->GetMethodID(file_dir_clz, "getAbsolutePath","()Ljava/lang/String;");
-    auto AbsolutePath = static_cast<jstring>(env->CallObjectMethod(file_dir, getAbsolutePath));
-    const char* path_str = env->GetStringUTFChars(AbsolutePath, JNI_FALSE);
-    std::string add_str(path_str);
-    add_str.append("/target/classes.dex");
-//    LOGE("AbsolutePath = %s",add_str.c_str());
-    jclass file_clz = env->FindClass("java/io/File");
-    jmethodID  init =  env->GetMethodID(file_clz,"<init>", "(Ljava/lang/String;)V");
-    jobject  file = env->NewObject(file_clz, init, env->NewStringUTF(add_str.c_str()));
-//    jmethodID  deleteFilesByDirectory  = env->GetStaticMethodID(clazz, "deleteFilesByDirectory", "(Ljava/io/File;)Z");
-//    env->CallStaticBooleanMethod(clazz, deleteFilesByDirectory, file);
 
-    jclass Tools_clz = env->FindClass("com/anygames/sdk/Tools");
-    jmethodID deleteFilesByDirectory  = env->GetStaticMethodID(Tools_clz, "deleteFilesByDirectory", "(Ljava/io/File;)Z");
-    env->CallStaticBooleanMethod(Tools_clz, deleteFilesByDirectory, file);
-
-    fakeDex(env, base, "conf");//dex collapse feature support
-
-    env->CallStaticBooleanMethod(Tools_clz, deleteFilesByDirectory, file);
-
-    env->DeleteLocalRef(file_dir);
-    env->DeleteLocalRef(file);
 }
+
+
+
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+JNIEnv *env;
+if ((*vm).GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) == JNI_OK) {
+}else{
+}
+global_jvm = vm;
+register_native(env);
+return onJniLoad(vm,reserved);
+}
+
